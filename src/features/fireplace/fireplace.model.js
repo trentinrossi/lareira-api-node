72df8a38fcde7601af37d03c478493b6b1c7eb36
Rodrigo Rossi <rdrg.rossi@gmail.com>
2021-05-05 09:57:00 -0300

    feature: Remove versioning in model

--- src/features/fireplace/fireplace.model.js
+++ src/features/fireplace/fireplace.model.js
@@ -3,20 +3,25 @@ const mongoose = require('mongoose');
 const mongooseDelete = require('mongoose-delete');
 const mongooseTimestamp = require('mongoose-timestamp');
 
-const schema = new mongoose.Schema({
-  name: { type: String, unique: true, required: true },
-  addess: { type: String },
-  neighborhood: { type: String },
-  postalCode: { type: String },
-  city: { type: String },
-  state: { type: String },
-  phone: { type: String },
-  //   createdBy: {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     ref: 'Subscriber',
-  //     default: null,
-  //   },
-});
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, unique: true, required: true },
+    addess: { type: String },
+    neighborhood: { type: String },
+    postalCode: { type: String },
+    city: { type: String },
+    state: { type: String },
+    phone: { type: String },
+    //   createdBy: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Subscriber',
+    //     default: null,
+    //   },
+  },
+  {
+    versionKey: false,
+  }
+);
 
 schema.plugin(mongooseDelete, { overrideMethods: true });
 schema.plugin(mongooseTimestamp);
